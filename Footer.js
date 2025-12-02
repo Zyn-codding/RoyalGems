1524ccd9b4fab6f18ac3e6477c360847faebf710
Zyn <simulationcarx@gmail.com>
2025-12-02 15:01:52 +0700

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', textAlign: 'center', backgroundColor: '#0f0f1e', marginTop: '20px' }}>
+      <p>RoyalGems &copy; 2025</p>
+    </footer>
+  );
+}
+
+export default Footer;
